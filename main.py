87713c60f0547df4a110cb4e4957a4c443237980
Harrison Hank <codinghank@gmail.com>
2024-06-19 09:45:34 +0300

    main file

---
+++ main.py
@@ -0,0 +1,10 @@
+import datetime
+
+DAYS = ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday',
+        'Friday', 'Saturday')
+
+MONTHS = ('January', 'February', 'March', 'April', 'May', 'June',
+          'July', 'August', 'September', 'October', 'November', 'December')
+
+print("Calender by Hank")
+
