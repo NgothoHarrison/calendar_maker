902c808b9f3a73a6444e5a2f62ea81e792b323a0
Harrison Hank <codinghank@gmail.com>
2024-06-19 10:04:25 +0300

    Enter the month

--- main.py
+++ main.py
@@ -14,3 +14,20 @@ while True:
     if response.isdecimal() and int(response) > 0:
         year = int(response)
         break
+    
+    print("Please enter a numeric year")
+    continue
+
+while True:
+    response = input("Enter the month : ")
+
+    if not response.isdecimal():
+        print("Enter a numeric month: ")
+        continue
+    
+    month = int(response)
+    if 1<= month <= 12:
+        break
+    
+    print("Enter a month between 1 and 12")
+    
